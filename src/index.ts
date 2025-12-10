14b30d415476984245c1172991b74e2aa9aa2753
nabte <ismanabte@gmail.com>
2025-12-10 08:56:56 -0600

    Fix: Convertir PORT a número para TypeScript

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ import apiRoutes from './api/routes';
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = parseInt(process.env.PORT || '3000', 10);
 
 // Middleware
 app.use(cors());
